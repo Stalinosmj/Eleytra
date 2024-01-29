5e5fd15b58ba1060f4272ba4a60c8c95dec7ac32
Stalino A J <93860427+Stalinosmj@users.noreply.github.com>
2024-01-29 11:39:00 +0530

    Update NavPage.dart

--- lib/presentation/screens/NavPage.dart
+++ lib/presentation/screens/NavPage.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutterflow_ui/flutterflow_ui.dart';
-import 'package:flutter_animate/flutter_animate.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:percent_indicator/percent_indicator.dart';
-import 'package:lottie/lottie.dart';
 
 
 
@@ -19,16 +14,21 @@ class _NavPage extends State<NavPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: SizedBox(
-          width: 50,
-          height: 50,
-          child: SpinKitCubeGrid(
-            color: FlutterFlowTheme.of(context).primary,
-            size: 50,
-          ),
-        ),
+    return const Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: Builder(builder: (context){
+                  final
+                }),
+              )
+            ],
+          )
+        ]
       ),
     )
   }
