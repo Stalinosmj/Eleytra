0c7b8a599ac6a4689a9de841dd8faca28fd5db7e
Stalino A J <93860427+Stalinosmj@users.noreply.github.com>
2024-02-02 16:05:49 +0530

    started navpage

--- lib/presentation/screens/NavPage.dart
+++ lib/presentation/screens/NavPage.dart
@@ -1,40 +1,51 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutterflow_ui/flutterflow_ui.dart';
-import 'package:lottie/lottie.dart';
-
-
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class NavPage extends StatefulWidget {
   const NavPage({Key? key}) : super(key: key);
-
   @override
-  _MyNavPage createState() => _MyNavPage();
+  _NavPageState createState() => _NavPageState();
 }
 
-class _MyNavPage extends State<NavPage> {
+class _NavPageState extends State<NavPage> {
+
+  // in the below line, we are initializing our controller for google maps.
+  Completer<GoogleMapController> _controller = Completer();
+
+  // in the below line, we are specifying our camera position
+  static final CameraPosition _kGoogle = const CameraPosition(
+    target: LatLng(37.42796133580664, -122.885749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Text(
-            'Loading...',
-            style: FlutterFlowTheme.of(context)
-            .bodyLarge
-            .override(
-            fontFamily: 'Outfit',
-            color: FlutterFlowTheme.of(context)
-                .alternate,
-            ),
+      // in the below line, we are specifying our app bar.
+        appBar: AppBar(
+          // setting background color for app bar
+          backgroundColor: const Color(0xFF0F9D58),
+          // setting title for app bar.
+          title: const Text("Charging Stations"),
+        ),
+        body: Container(
+          // in the below line, creating google maps.
+          child: GoogleMap(
+            // in the below line, setting camera position
+            initialCameraPosition: _kGoogle,
+            // in the below line, specifying map type.
+            mapType: MapType.normal,
+            // in the below line, setting user location enabled.
+            myLocationEnabled: true,
+            // in the below line, setting compass enabled.
+            compassEnabled: true,
+            // in the below line, specifying controller on map complete.
+            onMapCreated: (GoogleMapController controller){
+              _controller.complete(controller);
+            },
           ),
-          Lottie.network(
-            'https://lottie.host/acdb42dc-9774-41fd-bc69-d79cea3ba395/D63X5RQ0mV.json',
-            fit: BoxFit.fitHeight,
-          )
-        ],
-      ),
+        )
     );
   }
 }
