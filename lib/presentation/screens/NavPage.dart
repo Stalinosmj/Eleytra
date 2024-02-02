49a33ae1a298deeb6847e15ca7df683ce855ff0b
Stalino A J <93860427+Stalinosmj@users.noreply.github.com>
2024-02-02 13:57:01 +0530

    loading screen

--- lib/presentation/screens/NavPage.dart
+++ lib/presentation/screens/NavPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterflow_ui/flutterflow_ui.dart';
+import 'package:lottie/lottie.dart';
 
 
 
@@ -13,11 +14,24 @@ class NavPage extends StatefulWidget {
 class _MyNavPage extends State<NavPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/login_bg.png'), fit: BoxFit.cover)
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Text(
+          'Loading...',
+          style: FlutterFlowTheme.of(context)
+          .bodyLarge
+          .override(
+          fontFamily: 'Outfit',
+          color: FlutterFlowTheme.of(context)
+              .alternate,
+          ),
         ),
-      );
+        Lottie.network(
+          'https://lottie.host/4c6105e6-f01f-493c-a859-83db18c457af/UrC9pKBVXX.json',
+          fit: BoxFit.fill,
+        )
+      ],
+    );
   }
 }
